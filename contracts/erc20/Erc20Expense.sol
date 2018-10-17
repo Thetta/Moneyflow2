f764f28d520c3ea235933227e292d342d4d78718
enkogu <kirill@chain.cloud>
2018-10-17 20:43:28 +0800

    names for erc20 moneyflow contracts

--- contracts/erc20/Erc20Expense.sol
+++ contracts/erc20/Erc20Expense.sol
@@ -1,7 +1,10 @@
-pragma solidity ^0.4.15;
+pragma solidity ^0.4.24;
 
 import "../interfaces/IMoneyflow.sol";
 
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
+contract Erc20Expense {
+
+}
