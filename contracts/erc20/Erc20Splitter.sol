cd80ee72be27ceb80a6ce080c978277ed24b0470
enkogu <kirill@chain.cloud>
2018-10-17 21:00:10 +0800

    linter fix

--- contracts/erc20/Erc20Splitter.sol
+++ contracts/erc20/Erc20Splitter.sol
@@ -5,6 +5,7 @@ import "../interfaces/IMoneyflow.sol";
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
+
 contract Erc20Splitter {
 
 }
